82e54da4e99a32835e8daf6441c17d65960cc2e3
AndrePisco <91678659+AndrePisco@users.noreply.github.com>
2024-10-15 09:53:47 +0100

    Update sample_1.sql

--- sample_1.sql
+++ sample_1.sql
@@ -4,17 +4,17 @@ select
 
 	/******** ACTION FIELDS ********/
 	,'Test_Oct15' as process_name
-	,TO_JSON_STRING(STRUCT(
-        'Credit - TEST_TEST_TEST ' || creditor_id AS subject,
-        'Normal' AS priority,
-        3285009 AS brand_id,
-        360005611314 AS group_id,
-        9724439852828 AS requester_id,
-        5636997079964 AS ticket_form_id,
-        'This is a test\nPlease Ignore' AS commentBody,
-        false AS commentPublic,
-        'credit__inactive_company' AS category
-    )) AS ActionField_ZendeskCreateTicket
+	-- ,TO_JSON_STRING(STRUCT(
+ --        'Credit - TEST_TEST_TEST ' || creditor_id AS subject,
+ --        'Normal' AS priority,
+ --        3285009 AS brand_id,
+ --        360005611314 AS group_id,
+ --        9724439852828 AS requester_id,
+ --        5636997079964 AS ticket_form_id,
+ --        'This is a test\nPlease Ignore' AS commentBody,
+ --        false AS commentPublic,
+ --        'credit__inactive_company' AS category
+ --    )) AS ActionField_ZendeskCreateTicket
 
 
 from `gc-prd-risk-prod-gdia.dbt_risk.d_fds_exposure`
