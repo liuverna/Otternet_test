adc7e47b6f22290de0e69060459f70823971a606
AndrePisco <91678659+AndrePisco@users.noreply.github.com>
2024-10-16 10:58:58 +0100

    Update sample_1.sql

--- sample_1.sql
+++ sample_1.sql
@@ -12,9 +12,9 @@ select
   	,TO_JSON_STRING(STRUCT(
         STRUCT(
             "normal" AS priority, 
-            "brand_id": 3285009, -- Uncomment if you want to include
-            "group_id": 12439677670684, -- Uncomment if needed
-            "requester_id": 9724439852828, -- Uncomment if needed
+            -- "brand_id": 3285009, -- Uncomment if you want to include
+            -- "group_id": 12439677670684, -- Uncomment if needed
+            -- "requester_id": 9724439852828, -- Uncomment if needed
             12443238293916 AS ticket_form_id,
             ARRAY<STRUCT<
                 id INT64, 
@@ -22,9 +22,9 @@ select
             >>[
                 -- Custom field entries
                 -- Uncomment if needed
-                STRUCT(28480929, 'fraud__alerts_low'),  -- Category
+                -- STRUCT(28480929, 'fraud__alerts_low'),  -- Category
                 STRUCT(15542500163356, 12345)  -- Exposure
-                STRUCT(15545615128732, 123)  -- Fraud score (uncomment if needed)
+                -- STRUCT(15545615128732, 123)  -- Fraud score (uncomment if needed)
             ] AS custom_fields,
 
             -- Comment object
