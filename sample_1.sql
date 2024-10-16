f654f08a76d1ee5de23c03b4c020b9054739f76e
AndrePisco <91678659+AndrePisco@users.noreply.github.com>
2024-10-16 10:08:20 +0100

    Update sample_1.sql

--- sample_1.sql
+++ sample_1.sql
@@ -4,20 +4,34 @@ select
 
 	/******** ACTION FIELDS ********/
 	,'Test_Oct15' as process_name
-	,TO_JSON_STRING(STRUCT(
-        'Credit - TEST_TEST_TEST ' || creditor_id AS subject,
-        'Normal' AS priority,
-        3285009 AS brand_id,
-        360005611314 AS group_id,
-        9724439852828 AS requester_id,
-        5636997079964 AS ticket_form_id,
-        'This is a test\nPlease Ignore' AS commentBody,
-        false AS commentPublic,
-        'credit__inactive_company' AS category
+  	,TO_JSON_STRING(STRUCT(
+        STRUCT(
+            "normal" AS priority, 
+            -- "brand_id": 3285009, -- Uncomment if you want to include
+            -- "group_id": 12439677670684, -- Uncomment if needed
+            -- "requester_id": 9724439852828, -- Uncomment if needed
+            12443238293916 AS ticket_form_id,
+            ARRAY<STRUCT<
+                id INT64, 
+                value INT64
+            >>[
+                -- Custom field entries
+                -- Uncomment if needed
+                -- STRUCT(28480929, 'fraud__alerts_low'),  -- Category
+                STRUCT(15542500163356, 12345)  -- Exposure
+                -- STRUCT(15545615128732, 123)  -- Fraud score (uncomment if needed)
+            ] AS custom_fields,
+
+            -- Comment object
+            STRUCT(
+                'This is s test do not Panic!!!' AS body,
+                false AS public
+            ) AS comment,
+
+            -- Subject
+            'This is s test do not Panic!!! 1' AS subject
+        ) AS ticket
     )) AS ActionField_ZendeskCreateTicket
 
-
 from `gc-prd-risk-prod-gdia.dbt_risk.d_fds_exposure`
-LIMIT 2
-
-
+LIMIT 1
