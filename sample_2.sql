1108713d5cdf567cd0c2a567b71b1a2c4e5ecc64
AndrePisco <91678659+AndrePisco@users.noreply.github.com>
2024-10-22 11:34:15 +0100

    Update sample_2.sql

--- sample_2.sql
+++ sample_2.sql
@@ -1,11 +1,12 @@
 select 
 	creditor_id
 	,amount_gbp as fds_exposure_current
- ,3402624 as ticket_id
+ ,3417983 as ticket_id
 
 	/******** ACTION FIELDS ********/
 	,'Test_Oct15' as process_name
-	, TO_JSON_STRING(STRUCT(
+, TO_JSON_STRING(STRUCT(
+    STRUCT(
         12443238293916 AS ticket_form_id,
         ARRAY<STRUCT<
             id INT64, 
@@ -15,12 +16,15 @@ select
             STRUCT(15542500163356, 55555),  -- First custom field (category)
             STRUCT(15545615128732, 123)     -- Second custom field (fraud score)
         ] AS custom_fields,
-        
-        -- Adding comment body and comment public fields
-        'This is a test\nPlease Ignore\n Editing comment' AS commentBody,
-        false AS commentPublic
-    )) AS ActionField_ZendeskUpdateTicket
 
+        -- Comment object
+        STRUCT(
+            'This is a test editing the ticket' AS body,
+            false AS public
+        ) AS comment
+    ) AS ticket
+)) AS ActionField_ZendeskUpdateTicket
+	
 
 from `gc-prd-risk-prod-gdia.dbt_risk.d_fds_exposure`
 LIMIT 1
